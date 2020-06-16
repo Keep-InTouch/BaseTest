b1b0b13e9741b03df2f5f69c07e005ae0ed91d08
AARYAN KAPUR <ak6392@bennett.edu.in>
2020-06-16 23:47:27 +0530

    Updates

---
+++ precache-manifest.a42f928ffa7fbf5f9dec681550f1624d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "015a72403bb87ddfe1b059b1d482d495",
+    "url": "/Talk/index.html"
+  },
+  {
+    "revision": "ce9c7a5af569df9a568d",
+    "url": "/Talk/static/css/main.09b1df95.chunk.css"
+  },
+  {
+    "revision": "d2d846ffe6e47373e952",
+    "url": "/Talk/static/js/2.58e61f9f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Talk/static/js/2.58e61f9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce9c7a5af569df9a568d",
+    "url": "/Talk/static/js/main.2b0ad159.chunk.js"
+  },
+  {
+    "revision": "63db4c8a7eb68ea182a5",
+    "url": "/Talk/static/js/runtime-main.695cc4bd.js"
+  }
+]);
